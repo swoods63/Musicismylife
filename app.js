c6d582679aa7cf0cd3c60534db3e90ab30538ca8
Susan Woods <swoods63@msn.com>
2017-04-26 15:12:32 -0600

    Added column

--- app.js
+++ app.js
@@ -1,20 +1,35 @@
-function ItunesController(){
+function ItunesController() {
   var itunesService = new ItunesService()
   //Do Not Modify the getMusic function
-  this.getMusic = function getMusic(e){
+  this.getMusic = function getMusic(e) {
     e.preventDefault();
     var artist = e.target.artist.value;
     itunesService.getMusicByArtist(artist).then(drawSongs);
   }
 
-  function drawSongs(songList){
+  function drawSongs(songList) {
     console.log(songList);
     // This is where you task begins
-      
-    
-    
+    var songs = document.getElementById("song-list")
+    var template = ""
+    for (var i = 0; i < 50; i++) {
+      template += `
+  <div class="row">
+    <div class="col-lg-2"><img src = ${songList[i].albumArt}></div>
+    <div class="col-lg-2"><p>${songList[i].artist}</p></div>
+    <div class="col-lg-2"><p>${songList[i].title}</p></div>
+    <div class="col-lg-2"><p>${songList[i].collection}</p></div>
+    <div class="col-lg-2"><p>${songList[i].price}</p></div>
+    <div class="col-lg-2"><audio controls>
+    <source src="${songList[i].preview}" type="audio/ogg">
+    </audio></div>
+  </div>
+  `
+    }
+    songs.innerHTML = template
+
   }
-  
+
 }
 
 
